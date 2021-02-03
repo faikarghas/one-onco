71b8180a3a67c7622e99d9e002e6a0109a1de797
faikar <ghassanfaikar@yahoo.com>
2021-02-03 17:09:10 +0700

    add pwa to app

--- resources/views/v_home.blade.php
+++ resources/views/v_home.blade.php
@@ -12,6 +12,8 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('/css/main.css') }}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+        @laravelPWA
     </head>
     <body>
         <header>
