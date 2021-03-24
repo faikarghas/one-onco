725779cc10a64db58895568211b656ccd4f5ff44
faikar <ghassanfaikar@yahoo.com>
2021-03-24 15:58:39 +0700

    news list

--- resources/views/components/presentational/newsList.blade.php
+++ resources/views/components/presentational/newsList.blade.php
@@ -12,28 +12,10 @@
                                 'title'=>$row->title,
                                 'image_url'=>'https://source.unsplash.com/random',
                                 'description'=>$row->shortContent,
-                                'path'=>'berita-terkini'
+                                'path'=>'berita-terkini/'.$row->slug
                         ))
                     </div>
-                    @endforeach
-            <!-- <div class="col-12 col-lg-4">
-                @include('components/presentational.boxNews',array(
-                    'date'=>'24 Nov 2020',
-                    'title'=>'Perbandingan biaya kemotrapi antara indonesia & Malaysia 2020',
-                    'image_url'=>'https://source.unsplash.com/random',
-                    'description'=>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam perspiciatis dolor rem blanditiis. Vitae veniam, aliquid molestias non nostrum',
-                    'path'=>'/berita-terkini/test'
-                ))
-            </div>
-            <div class="col-12 col-lg-4">
-                @include('components/presentational.boxNews',array(
-                    'date'=>'24 Nov 2020',
-                    'title'=>'Perbandingan biaya kemotrapi antara indonesia & Malaysia 2020',
-                    'image_url'=>'https://source.unsplash.com/random',
-                    'description'=>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam perspiciatis dolor rem blanditiis. Vitae veniam, aliquid molestias non nostrum',
-                    'path'=>'/berita-terkini/test'
-                ))
-            </div> -->
+            @endforeach
             <div class="col-12 text-center mt-5">
                 @include('components/presentational.boxShowMore',array(
                     'title'=>'Tampilkan lainnya',
