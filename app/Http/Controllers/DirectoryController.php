512dc398e1d0f48635c1e73d83eaa7fa288e714a
faikar <ghassanfaikar@yahoo.com>
2021-03-24 10:01:29 +0700

    [edit] home

--- app/Http/Controllers/DirectoryController.php
+++ app/Http/Controllers/DirectoryController.php
@@ -15,7 +15,7 @@ class DirectoryController extends Controller
     public function index(Request $request){
 
       // GET variable from global data for website
-      $siteConfig   = DB::table('global_data')->first();    
+      $siteConfig   = DB::table('global_data')->first();
       // check sebagai customer apa bukan
       if(Session()->get('username')=="") {
         $statusLogin = "<a href='/login'>LOGIN</a>";
@@ -32,7 +32,7 @@ class DirectoryController extends Controller
     public function dokter(){
 
       // GET variable from global data for website
-      $siteConfig   = DB::table('global_data')->first();    
+      $siteConfig   = DB::table('global_data')->first();
       // check sebagai customer apa bukan
       if(Session()->get('username')=="") {
         $statusLogin = "<a href='/login'>LOGIN</a>";
@@ -46,7 +46,7 @@ class DirectoryController extends Controller
                     'copyright'=>$siteConfig->pvar3,
                     'statusLogin'=>$statusLogin
                   );
-      
+
       return view ('v_direktoriDokter', $data,compact('cities'));
     }
 
@@ -60,12 +60,12 @@ class DirectoryController extends Controller
       // DB::table('artikel')->where('idKat',2)->limit(3)->orderBy('id', 'DESC')->get();
       // $viewDokter = DB::table("dokter_mapped")->where("provinsi",$id)->distinct('dokterId','provinsi','kabupaten')->get();
       $viewDokter = DB::table("dokter_mapped")->select('dokterId', 'NamaDokterDenganGelar', 'unit' )->where("provinsi",$id)->distinct()->get();
-      
+
       return response()->json($viewDokter);
-      // return json_encode($viewDokter);  
+      // return json_encode($viewDokter);
   }
 
-  
+
   public function getFaskes($id) {
 
     $viewFaskes = DB::table("faskes")
@@ -73,7 +73,7 @@ class DirectoryController extends Controller
     ->select('faskesId', 'NamaFaskes', 'alamat','kodepos', 'phone','fax', 'email','website', 'indonesia_cities.name AS propinsi')
     ->where("provinsi",$id)
     ->get();
-    return response()->json($viewFaskes);  
+    return response()->json($viewFaskes);
 }
 
 public function getFaskesWithPropinsi($id) {
@@ -89,23 +89,23 @@ public function getFaskesWithKabupaten($id) {
     ->select('faskesId', 'NamaFaskes', 'alamat','kodepos', 'phone','fax', 'email','website', 'indonesia_cities.name AS propinsi')
     ->where("kabupaten",$id)
     ->get();
-    return response()->json($viewFaskes);  
+    return response()->json($viewFaskes);
 }
 
   public function getDokterDetail($id, Request $request) {
 
 
-    $siteConfig   = DB::table('global_data')->first();   
-    
+    $siteConfig   = DB::table('global_data')->first();
+
     // GET variable from global data for website
-    $siteConfig   = DB::table('global_data')->first();    
+    $siteConfig   = DB::table('global_data')->first();
     // check sebagai customer apa bukan
     if(Session()->get('username')=="") {
       $statusLogin = "<a href='/login'>LOGIN</a>";
     } else {
       $statusLogin = "<a href='/logout'>LOGOUT</a>";
     }
-    
+
 
     // Dokter Detail
     $dokterDetail = DB::table('dokter')
@@ -114,13 +114,13 @@ public function getFaskesWithKabupaten($id) {
       ->select('dokter.id', 'dokter.fullname', 'faskes_layanan.name','jadwal_dokter.jadwal')
       ->where('dokter.id',$id)
       ->first();
-     
+
       $fullname = $dokterDetail->fullname;
       $layanan = $dokterDetail->name;
       //$fullname = $dokterDetail->fullname;
 
     // List Tempat Dokter Praktek
-    
+
     $idDokter = $id;
     // $faskesModel  = new Faskes_model();
     // $listFaskes  = $faskesModel->allByDokter($idDokter);
@@ -130,11 +130,11 @@ public function getFaskesWithKabupaten($id) {
         ->distinct('faskes.namaFaskes')
         ->where('jadwal_dokter.dokterId', $idDokter)->get();
         // ->dump();
-  
+
 
 
     //var_dump ($listFaskes);
-    
+
     $data = array('title' => $siteConfig->pvar2,
                     'copyright'=>$siteConfig->pvar3,
                     'fullname'=>$fullname,
@@ -145,8 +145,8 @@ public function getFaskesWithKabupaten($id) {
     return view ('v_direktoriDokterDetail', $data);
 
 
-    
-   
+
+
 }
 
   public function getDokterWithKabupaten($id) {
@@ -154,15 +154,15 @@ public function getFaskesWithKabupaten($id) {
     // DB::table('artikel')->where('idKat',2)->limit(3)->orderBy('id', 'DESC')->get();
     // $viewDokter = DB::table("dokter_mapped")->where("provinsi",$id)->distinct('dokterId','provinsi','kabupaten')->get();
     $viewDokter = DB::table("dokter_mapped")->select('dokterId', 'NamaDokterDenganGelar' )->where("kabupaten",$id)->distinct()->get();
-    
+
     return response()->json($viewDokter);
-    // return json_encode($viewDokter);  
+    // return json_encode($viewDokter);
 }
 
 public function lab(){
 
   // GET variable from global data for website
-  $siteConfig   = DB::table('global_data')->first();    
+  $siteConfig   = DB::table('global_data')->first();
   // check sebagai customer apa bukan
   if(Session()->get('username')=="") {
     $statusLogin = "<a href='/login'>LOGIN</a>";
@@ -176,7 +176,7 @@ public function lab(){
   // main page
   $data = array('title' => $siteConfig->pvar2,
                 'copyright'=>$siteConfig->pvar3,
-                'statusLogin'=>$statusLogin         
+                'statusLogin'=>$statusLogin
               );
   return view ('v_direktoriLab', $data,compact('provinces'));
 }
@@ -184,7 +184,7 @@ public function lab(){
 public function carehome(){
 
   // GET variable from global data for website
-  $siteConfig   = DB::table('global_data')->first();    
+  $siteConfig   = DB::table('global_data')->first();
   // check sebagai customer apa bukan
   if(Session()->get('username')=="") {
     $statusLogin = "<a href='/login'>LOGIN</a>";
@@ -198,7 +198,7 @@ public function carehome(){
   // main page
   $data = array('title' => $siteConfig->pvar2,
                 'copyright'=>$siteConfig->pvar3,
-                'statusLogin'=>$statusLogin         
+                'statusLogin'=>$statusLogin
               );
   return view ('v_direktoriCare', $data,compact('provinces'));
 }
@@ -206,7 +206,7 @@ public function carehome(){
   public function care($id,Request $request){
 
     // GET variable from global data for website
-    $siteConfig   = DB::table('global_data')->first();    
+    $siteConfig   = DB::table('global_data')->first();
     // check sebagai customer apa bukan
     if(Session()->get('username')=="") {
       $statusLogin = "<a href='/login'>LOGIN</a>";
@@ -220,7 +220,7 @@ public function carehome(){
         ->select('faskes.faskesId','faskes.namaFaskes', 'faskes.alamat', 'faskes.provinsi', 'faskes.kabupaten', 'faskes.website','faskes.phone','faskes.fax')
         ->where('faskes.faskesId', $id)->first();
         // ->dump();
-    
+
     $namaFaskes = $viewFaskes->namaFaskes;
     $addressFaskes = $viewFaskes->alamat;
     $phoneFaskes = $viewFaskes->phone;
@@ -235,9 +235,9 @@ public function carehome(){
     ->where('jadwal_dokter.faskesId', $id)
     ->get();
     //->dump();
-    
 
-    
+
+
 
 
     // main page
@@ -250,12 +250,12 @@ public function carehome(){
                   'phone'=>$phoneFaskes,
                   'website'=>$websiteFaskes,
                   'viewDokter' => $viewDokter
-                  
+
                 );
     return view ('v_direktoriCareDetail', $data);
   }
 
 
-   
+
 
 }
