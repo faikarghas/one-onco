c8aac98f5410d2d975dd80bb6b8000b449f4fd3b
faikar <ghassanfaikar@yahoo.com>
2021-03-24 19:31:26 +0700

    init

--- resources/views/v_jurnalOnkologi.blade.php
+++ resources/views/v_jurnalOnkologi.blade.php
@@ -16,22 +16,17 @@
                     <div class="col-12">
                         <h2 class="text-center mb-5"><strong>ARTIKEL KANKER</strong></h2>
                     </div>
-                    <div class="col-12 col-md-4">
-                        @include('components/presentational.boxNews',array(
-                            'date'=>'24 Nov 2020',
-                            'title'=>'Perbandingan biaya kemotrapi antara indonesia & Malaysia 2020',
-                            'image_url'=>'https://source.unsplash.com/random',
-                            'path'=>'jurnal-onkologi/test'
-                        ))
-                    </div>
-                    <div class="col-12 col-md-4">
-                        @include('components/presentational.boxNews',array(
-                            'date'=>'24 Nov 2020',
-                            'title'=>'Perbandingan biaya kemotrapi antara indonesia & Malaysia 2020',
-                            'image_url'=>'https://source.unsplash.com/random',
-                            'path'=>'jurnal-onkologi/test'
-                        ))
-                    </div>
+                    @foreach($listingJurnal as $row)
+                        <div class="col-12 col-md-4">
+                            @include('components/presentational.boxNews',array(
+                                'date'=> $row->createdAt,
+                                'title'=>$row->title ,
+                                'image_url'=>'https://source.unsplash.com/random',
+                                'description'=>$row->shortContent,
+                                'path'=>'artikel-kanker/'.$row->slug
+                            ))  
+                        </div>
+                    @endforeach
                     <div class="col-12 text-center mt-5">
                         @include('components/presentational.boxShowMore',array(
                             'title'=>'Tampilkan lainnya',
