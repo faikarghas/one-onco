694fd19ef57f90bad81e87db975193872c7548a0
faikar <ghassanfaikar@yahoo.com>
2021-04-27 12:31:31 +0700

    [add] konsultasi & belanja online

---
+++ resources/views/v_belanjaSehat.blade.php
@@ -0,0 +1,15 @@
+@extends('components/layouts.layout')
+
+@section('content')
+    @include('components/presentational/header',['path'=>''])
+
+    <main>
+        <section class="belanja__page">
+            <div class="container">
+                <div class="row">
+                    <iframe src="" frameborder="0"></iframe>
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
