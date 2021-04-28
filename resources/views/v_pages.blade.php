a84a9c904a12b3a4b9d7d115fdab6d29370c5d93
faikar <ghassanfaikar@yahoo.com>
2021-04-28 19:22:56 +0700

    [fix] detail artikel

--- resources/views/v_pages.blade.php
+++ resources/views/v_pages.blade.php
@@ -35,6 +35,28 @@
                             <h3>{!! $titleContentPages !!}</h3>
                             <p>{!! $contentPages !!}</p>
                         </div>
+                        <div class="share_sosmed">
+                            <div class="share_sosmed-link">
+                                <p>Apabila informasi ini berguna untuk Anda, <br>bagikan ke keluarga, kerabat, dan teman Anda.</p>
+                                <a href="https://facebook.com/sharer/sharer.php?u={{url()->full()}}" target="_blank" rel="noopener"">
+                                    <img src="{{asset('/images/fb2-logo.png')}}" alt="fb-logo" width="30px">
+                                </a>
+                                <a href="https://twitter.com/intent/tweet/?text=Super%20fast%20and%20easy%20Social%20Media%20Sharing%20Buttons.%20No%20JavaScript.%20No%20tracking.&amp;url={{url()->full()}}" target="_blank" rel="noopener" aria-label="">
+                                    <img src="{{asset('/images/twitter2-logo.png')}}" alt="twitter-logo" width="30px">
+                                </a>
+                                <a href="https://wa.me/?text={{url()->full()}}" target="_blank" rel="noopener">
+                                    <img src="{{asset('/images/wa-logo.png')}}" alt="fb-logo" width="30px">
+                                </a>
+                            </div>
+                            <div class="share_sosmed-act">
+                                <ul>
+                                    <li>
+                                        <a href="/belanja-sehat"><img src="{{asset('/images/belanja_sehat_icon.png')}}" alt="logo belanja sehat"></a>
+                                        <a href="/konsultasi-online"><img src="{{asset('/images/konsultasi_online_icon.png')}}" alt="logo belanja sehat"></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
