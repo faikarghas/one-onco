1419810155fb303699bd6cedff7da8712cacfda7
faikar <ghassanfaikar@yahoo.com>
2021-04-29 17:15:05 +0700

    [fix] direktori

--- resources/views/components/presentational/boxDirectoryFaskesDetail.blade.php
+++ resources/views/components/presentational/boxDirectoryFaskesDetail.blade.php
@@ -13,11 +13,24 @@
                             <div class="col-9 d-flex flex-column align-items-start">
                                 <h3><strong>{{ $name }}</strong></h3>
                                 <ul>
-                                    <li><p>{{ $address }}<br></p></li>
-                                    <li class="mt-3"><p>{{ $phone }}</p></li>
-                                    <li><p>{{ $fax }}</p></li>
+                                    <li class="d-flex align-items-start">
+                                        <img class="me-4" src="{{asset('images/placeholder.svg')}}" width="14px" alt="icon web">
+                                        <p>{{ $address }}<br></p>
+                                    </li>
+                                    <li class="mt-3 d-flex align-items-start">
+                                        <img class="me-4" src="{{asset('images/phone-call.svg')}}" width="14px" alt="icon web">
+                                        <p>{{ $phone }}</p>
+                                    </li>
+                                    <li class="d-flex align-items-start mb-4">
+                                        <img class="me-4" src="{{asset('images/phone-call.svg')}}" width="14px" alt="icon web">
+                                        <p>{{ $fax }}</p>
+                                    </li>
+                                    <li class="d-flex align-items-start">
+                                        <img class="me-4" src="{{asset('images/global.svg')}}" width="14px" alt="icon web">
+                                        <a href="" style="color: #00A2E3">{{ $website }}</a>
+                                    </li>
                                 </ul>
-                                <a class="mt-3" href="" style="color: #00A2E3">{{ $website }}</a>
+                                {{-- <img src="{{asset('images/global.svg')}}" alt=""> --}}
                             </div>
                         </div>
                     </div>
