2cbafb6f1d46b183b17ae4956bc2499ef622284c
faikar <ghassanfaikar@yahoo.com>
2021-05-04 13:23:38 +0700

    [fix] date, disclaimer & autoplay slider partner

--- resources/views/v_direktoriCare.blade.php
+++ resources/views/v_direktoriCare.blade.php
@@ -39,10 +39,10 @@
         <section class="direktori__list">
             @include('components/presentational/boxFilterDirectoryDesktop',['path'=>'direktori'])
             @include('components/presentational/boxResultFilterDirectoryFaskes',['path'=>'direktori'])
-            <div class="container">
+            <div class="container text-center mt-3">
                 <div class="row">
                     <div class="col-12 ">
-                        <p style="color:#c3c2c2;">Segala konten yang diterbitkan/ publikasikan hanya ditujukan untuk kepentingan penyampaian informasi kepada public. Jadwal dan informasi terkait layanan dapat berubah sewaktu-waktu tanpa pemberitahuan. Seluruh pengguna diharapkan untuk mengkonfirmasi jadwal dengan layanan sebelumnya.</p>
+                        <p style="color:#c3c2c2;">Segala konten yang diterbitkan/ publikasikan hanya ditujukan untuk kepentingan penyampaian informasi kepada public. Jadwal dan informasi terkait layanan dapat berubah sewaktu-waktu tanpa pemberitahuan. <br/> Seluruh pengguna diharapkan untuk mengkonfirmasi jadwal dengan layanan sebelumnya.</p>
                     </div>
                 </div>
             </div> 
