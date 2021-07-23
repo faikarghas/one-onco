c47ab4561b6ad4b8efdb426b52de1db62e756c3c
Faikar <ghassanfaikar@yahoo.com>
2021-07-23 19:40:02 +0700

    [update] layout belanja-page

--- resources/views/v_belanjaSehat.blade.php
+++ resources/views/v_belanjaSehat.blade.php
@@ -4,7 +4,7 @@
     @include('components/presentational/header',['path'=>''])
     <main>
         <section class="belanja__page">
-            <div class="container">
+            <div class="container-fluid">
                 <div class="row">
                     {{-- <iframe src="https://healthmall.medkomtek.net/products/category/suplemen-vitamin?kdUser={{ session()->get('tokenUser') }}"" frameborder="0" width="100%" height="600" target="_parent" ></iframe> --}}
                     <iframe src="https://healthmall.klikdokter.com/products/category/kanker?kdUser={{ session()->get('tokenUser') }}"" frameborder="0" width="100%" height="600" target="_parent" ></iframe>
