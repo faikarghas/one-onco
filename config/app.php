2b71523f13b40881e5cc241138ff3f03386ef458
khoerul-adhayana <khoeruladhayana@gmail.com>
2021-07-25 16:04:46 +0700

    updated

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://one-onco.herokuapp.com/'),
+    'url' => env('APP_URL', 'https://oneonco.co.id/'),
 
     'asset_url' => env('ASSET_URL', null),
 
