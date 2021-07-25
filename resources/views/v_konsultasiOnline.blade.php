ce168689a6e4fb5998d9c020f136fb21914cdad3
khoerul adhayana <khoeruladhayana@gmail.com>
2021-07-25 16:13:02 +0700

    updated

--- resources/views/v_konsultasiOnline.blade.php
+++ resources/views/v_konsultasiOnline.blade.php
@@ -6,7 +6,8 @@
             <div class="container">
                 <div class="row">
                     {{-- <iframe src="https://livechat.medkomtek.net?kdUser={{ session()->get('tokenUser') }}" frameborder="0" width="100%" height="600" ></iframe> --}}
-                    <iframe src="https://livechat.klikdokter.com/?filter=124,38,9&kdUser={{ session()->get('tokenUser') }}" frameborder="0" width="100%" height="600" ></iframe>
+                    {{--<iframe src="https://livechat.klikdokter.com/?filter=124,38,9&kdUser={{ session()->get('tokenUser') }}" frameborder="0" width="100%" height="600" ></iframe>--}}
+			<iframe src="https://livechat.medkomtek.net?kdUser={{ session()->get('tokenUser') }}" frameborder="0" width="100%" height="600" ></iframe>
                 </div>
             </div>
         </section>
