c597ad3f7faf2f6895dbeb9721a72ad3d26870fd
khoerul-adhayana <khoeruladhayana@gmail.com>
2021-07-26 14:22:17 +0700

    updated v_belanjaSehat

--- resources/views/v_belanjaSehat.blade.php
+++ resources/views/v_belanjaSehat.blade.php
@@ -1,12 +1,11 @@
 @extends('components/layouts.layout')
-
 @section('content')
     @include('components/presentational/header',['path'=>''])
     <main>
         <section class="belanja__page">
             <div class="container-fluid">
                 <div class="row">
-                    <iframe id="belanja_sehat_iframe" src="https://healthmall.medkomtek.net/products/category/suplemen-vitamin?kdUser={{ session()->get('tokenUser') }}" frameborder="0" width="100%" height="800" target="_parent" ></iframe>
+                    <iframe id="belanja_sehat_iframe" src="https://healthmall.medkomtek.net/products/category/kanker?kdUser={{ session()->get('tokenUser') }}" frameborder="0" width="100%" height="800" target="_parent" ></iframe>
                 </div>
             </div>
         </section>
