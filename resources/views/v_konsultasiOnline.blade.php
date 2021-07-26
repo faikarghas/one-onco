8dbab7bdbcf1aed4fc7a3063f5f160aced1d790e
Faikar <ghassanfaikar@yahoo.com>
2021-07-26 17:16:50 +0700

    [update]

--- resources/views/v_konsultasiOnline.blade.php
+++ resources/views/v_konsultasiOnline.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     @include('components/presentational/header',['path'=>''])
     <main>
-        <section class="konsultasi__page">
-            <div class="container">
+        <section class="konsultasi__page pe-0 ps-0">
+            <div class="container-fluid">
                 <div class="row">
                     <iframe src="https://livechat.klikdokter.com/?filter=124,38,9?kdUser={{ session()->get('tokenUser') }}" frameborder="0" width="100%" height="600" ></iframe>
                 </div>
