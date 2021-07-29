24e6a74ef7da34823b11531e53f8649f85bc0f1b
khoerul-adhayana <khoeruladhayana@gmail.com>
2021-07-29 17:08:07 +0700

    updated

--- resources/views/v_indexChat.blade.php
+++ resources/views/v_indexChat.blade.php
@@ -9,70 +9,70 @@
                         <h1><strong>Jadwal Konsultasi Reguler</strong></h1>
                         <div class="row konsultasi__page-item small">
                             <div class="col-6 col-md-6 tx">Seputar Kanker</div>
-                            <div class="col-2 col-md-2 tx text-center">Senin-Minggu</div>
-                            <div class="col-2 col-md-2 tx text-end"></div>
-                            <div class="col-2 col-md-2 tx text-end">00:00-23:59</div>
+                            <div class="col-2 col-md-2 tx text-left">Senin-Minggu</div>
+                            <div class="col-2 col-md-2 tx text-left"></div>
+                            <div class="col-2 col-md-2 tx text-left">00:00-23:59</div>
                         </div>
                         <div class="row konsultasi__page-item">
                             <div class="col-6 col-md-6 tx">Seputar Penyakit Dalam</div>
-                            <div class="col-2 col-md-2 tx text-center">Senin-Minggu</div>
-                            <div class="col-2 col-md-2 tx text-end"></div>
-                            <div class="col-2 col-md-2 tx text-end">00:00-23:59</div>
+                            <div class="col-2 col-md-2 tx text-left">Senin-Minggu</div>
+                            <div class="col-2 col-md-2 tx text-left"></div>
+                            <div class="col-2 col-md-2 tx text-left">00:00-23:59</div>
                         </div>
                         <div class="row konsultasi__page-item">
                             <div class="col-6 col-md-6 tx">Seputar Psikologi </div>
-                            <div class="col-2 col-md-2 tx text-center">Senin-Minggu</div>
-                            <div class="col-2 col-md-2 tx text-end"></div>
-                            <div class="col-2 col-md-2 tx text-end">00:00-23:59</div>
+                            <div class="col-2 col-md-2 tx text-left">Senin-Minggu</div>
+                            <div class="col-2 col-md-2 tx text-left"></div>
+                            <div class="col-2 col-md-2 tx text-left">00:00-23:59</div>
                         </div>
                          
                         <h1 class="mt-5"><strong>Jadwal Konsultasi Spesialis Onkologi</strong></h1>
                         <div class="row konsultasi__page-item small">
                             <div class="col-6 col-md-6 tx small">dr. David Samuel Kereh, Sp. B (K) Onk</div>
-                            <div class="col-2 col-md-2 tx text-center">Bedah Onkologi</div>
-                            <div class="col-2 col-md-2 tx text-end">Senin & Kamis</div>
-                            <div class="col-2 col-md-2 tx text-end">17:00-18:00</div>
+                            <div class="col-2 col-md-2 tx text-left">Bedah Onkologi</div>
+                            <div class="col-2 col-md-2 tx text-left">Senin & Kamis</div>
+                            <div class="col-2 col-md-2 tx text-left">17:00-18:00</div>
                         </div>
                         <div class="row konsultasi__page-item">
                             <div class="col-6 col-md-6 tx">dr. Dedyanto Jenky Saputra, M.Gizi</div>
-                            <div class="col-2 col-md-2 tx text-center">Gizi</div>
-                            <div class="col-2 col-md-2 tx text-end">Senin-Minggu</div>
-                            <div class="col-2 col-md-2 tx text-end">19:00-21:00</div>
+                            <div class="col-2 col-md-2 tx text-left">Gizi</div>
+                            <div class="col-2 col-md-2 tx text-left">Senin-Minggu</div>
+                            <div class="col-2 col-md-2 tx text-left">19:00-21:00</div>
                         </div>
                         <div class="row konsultasi__page-item">
                             <div class="col-6 col-md-6 tx">dr. Ericko Ekaputra, Sp. Onk. Rad</div>
-                            <div class="col-2 col-md-2 tx text-center">Radiasi Onkologi</div>
-                            <div class="col-2 col-md-2 tx text-end">Senin-Jumat</div>
-                            <div class="col-2 col-md-2 tx text-end">17:00-19:00</div>
+                            <div class="col-2 col-md-2 tx text-left">Radiasi Onkologi</div>
+                            <div class="col-2 col-md-2 tx text-left">Senin-Jumat</div>
+                            <div class="col-2 col-md-2 tx text-left">17:00-19:00</div>
                         </div>
                         <div class="row konsultasi__page-item">
                             <div class="col-6 col-md-6 tx">dr. Gumilang Wiranegara, Sp.OG (K) Onk</div>
-                            <div class="col-2 col-md-2 tx text-center">Obgyn Onkologi</div>
-                            <div class="col-2 col-md-2 tx text-end">Senin</div>
-                            <div class="col-2 col-md-2 tx text-end">11:00-12:00</div>
+                            <div class="col-2 col-md-2 tx text-left">Obgyn Onkologi</div>
+                            <div class="col-2 col-md-2 tx text-left">Senin</div>
+                            <div class="col-2 col-md-2 tx text-left">11:00-12:00</div>
                         </div>
                         <div class="row konsultasi__page-item">
                             <div class="col-6 col-md-6 tx">dr. Hastarita Lawrenti</div>
-                            <div class="col-2 col-md-2 tx text-center">Onkologi</div>
-                            <div class="col-2 col-md-2 tx text-end">Selasa, Rabu, & Kamis</div>
-                            <div class="col-2 col-md-2 tx text-end">18:30-20:30</div>
+                            <div class="col-2 col-md-2 tx text-left">Onkologi</div>
+                            <div class="col-2 col-md-2 tx text-left">Selasa, Rabu, & Kamis</div>
+                            <div class="col-2 col-md-2 tx text-left">18:30-20:30</div>
                         </div>
                         <div class="row konsultasi__page-item">
                             <div class="col-6 col-md-6 tx">dr. Lucky Taufika Yuhedi, Sp. Onk.Rad</div>
-                            <div class="col-2 col-md-2 tx text-center">Radiasi Onkologi</div>
-                            <div class="col-2 col-md-2 tx text-end">Selasa</div>
-                            <div class="col-2 col-md-2 tx text-end">19:30-21:00</div>
+                            <div class="col-2 col-md-2 tx text-left">Radiasi Onkologi</div>
+                            <div class="col-2 col-md-2 tx text-left">Selasa</div>
+                            <div class="col-2 col-md-2 tx text-left">19:30-21:00</div>
                         </div>
                         <div class="row konsultasi__page-item">
                             <div class="col-6 col-md-6 tx">dr. Reza Musmarliansyah, Sp. B (K) Onk</div>
-                            <div class="col-2 col-md-2 tx text-center">Bedah Onkologi</div>
-                            <div class="col-2 col-md-2 tx text-end">Selasa & Kamis</div>
-                            <div class="col-2 col-md-2 tx text-end">13:00-14:00</div>
+                            <div class="col-2 col-md-2 tx text-left">Bedah Onkologi</div>
+                            <div class="col-2 col-md-2 tx text-left">Selasa & Kamis</div>
+                            <div class="col-2 col-md-2 tx text-left">13:00-14:00</div>
                         </div>
                     </div>
                     
-                    <div class="col-12 col-lg-5 rg">
-                            <div class="box_jadwal">
+                    <div class="col-12 col-lg-5 rg" style="align-items: flex-start;">
+                            <div class="box_jadwal" style="padding:5rem; width:80%;">
                                 <div class="row">
                                     <div class="col-12" style="font-size:1.8rem;font-weight:bold">
                                         Konsultasi seputar kesehatan & kanker sekarang!
