ca509790217bc50260cc4f259f112699494fc00e
khoerul-adhayana <khoeruladhayana@gmail.com>
2021-12-13 21:59:28 +0700

    updated

--- app/Http/Controllers/DirectoryController.php
+++ app/Http/Controllers/DirectoryController.php
@@ -75,11 +75,14 @@ class DirectoryController extends Controller
 
     public function getMoreFaskes(Request $request){
       $query = strtolower($request->search_query);
-      $spesialis = preg_replace("/[^A-Za-z0-9]/", "", $request->spesialis);
+      //$spesialis = preg_replace("/[^A-Za-z0-9]/", "", $request->spesialis);
+      $spesialis = $request->spesialis;
       $layanan = DB::table('faskes_layanan')->where('title', $spesialis)->first();
       dd($layanan);
+
+
       $spesialis =  $layanan->id;
-      
+
       $provinsi = $request->provinsi;
       //dd($provinsi);
       $kabupaten = $request->kabupaten;
